08ca36f913597fc3b2c23aed90a8e736747a81b2
Ace <angelacaurienglish@csus.edu>
2018-05-31 12:07:09 -0700

    Incorporated magic item template

--- resources/popupimg.js
+++ resources/popupimg.js
@@ -13,7 +13,10 @@ function initAll()
 
 function myNewWindow()
 {
-var littleWindow = window.open(this.href, "ltWin", "toolbar=no, location=no, status=yes, menubar=no, scrollbars=yes, width=768px, height=576px, left=500px, top=300px");
+var littleWindow = 
+	window.open(this.href, "ltWin", 
+	"toolbar=no, location=no, status=yes, menubar=no, scrollbars=yes, ");
+	/*width=768px, height=576px, left=500px, top=300px*/
 littleWindow.focus();
 return false;
 }
